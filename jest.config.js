e31e0d835e6b4b72a7fc521a71e9733001b346db
Alan <alanscodelog@gmail.com>
2020-12-05 19:30:04 -0300

    build: updated template

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,9 @@
+/* eslint-disable import/no-extraneous-dependencies */
 // https://jestjs.io/docs/en/configuration.html
 
-// maps the path aliases correctly
-const tsc = require("typescript")
-const tsconfig = tsc.readConfigFile("tsconfig.json", tsc.sys.readFile);
-const { pathsToModuleNameMapper } = require('ts-jest/utils')
-const moduleNameMapper = pathsToModuleNameMapper(tsconfig.config.compilerOptions.paths, { prefix: '<rootDir>/' })
 
-
+/** @type {import('@jest/types').Config.InitialOptions} */
 module.exports = {
-	preset: "ts-jest",
 	// this will remove jsdom, which has an issue with node 10 (https://github.com/jsdom/jsdom/issues/2961)
 	// it also makes tests faster
 	testEnvironment: "node",
@@ -16,29 +11,21 @@ module.exports = {
 		"ts",
 		"js",
 		"json",
-		"node"
 	],
-	"transform": {
-		"^.+\\.(t)sx?$": "ts-jest",
+	transform: {
+		"^.+\\.(ts|js)$": "babel-jest",
 	},
 	transformIgnorePatterns: [
-		"/node_modules/"
+		"/node_modules/",
 	],
-	moduleNameMapper,
 	testMatch: [
-		"**/tests/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)"
+		"**/tests/**/*.spec.(js|ts)|**/__tests__/*.(js|ts)",
 	],
 	testURL: "http://localhost/",
 	watchPlugins: [
 		"jest-watch-typeahead/filename",
-		"jest-watch-typeahead/testname"
+		"jest-watch-typeahead/testname",
 	],
-	globals: {
-		"ts-jest": {
-			babelConfig: true,
-			diagnostics: false,
-		}
-	},
 	collectCoverageFrom: [
 		"**/src/**/*.ts",
 		"!**/node_modules/**",
@@ -48,4 +35,4 @@ module.exports = {
 		".*?/index.ts",
 	],
 	coverageDirectory: "coverage",
-};
+}
